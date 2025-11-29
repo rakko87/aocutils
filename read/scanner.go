63f1ed89b7b3e0403fe178cbf71ae4247c844776
tag <gninetag@gmail.com>
2025-11-29 20:41:50 +0100

    advent of code utils

---
+++ read/scanner.go
@@ -0,0 +1,24 @@
+package read
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"iter"
+)
+
+func scan(r io.Reader, f bufio.SplitFunc) iter.Seq2[int, string] {
+	scanner := bufio.NewScanner(r)
+	scanner.Split(f)
+	return func(yield func(int, string) bool) {
+		for i := 0; scanner.Scan(); i++ {
+			if !yield(i, scanner.Text()) {
+				return
+			}
+			if err := scanner.Err(); err != nil {
+				yield(i, fmt.Sprintf("error reading input %v", err))
+				return
+			}
+		}
+	}
+}
---
+++ read/scanner_test.go
@@ -0,0 +1,32 @@
+package read
+
+import (
+	"bufio"
+	"fmt"
+	"slices"
+	"strings"
+	"testing"
+)
+
+func Test_scan(t *testing.T) {
+	input := strings.NewReader("1\n2")
+	want := []string{"1", "2"}
+
+	got := make([]string, 0)
+	for _, line := range scan(input, bufio.ScanLines) {
+		got = append(got, line)
+	}
+	if !slices.Equal(got, want) {
+		t.Errorf("got %v want %v", got, want)
+	}
+}
+
+func Example_scan() {
+	input := strings.NewReader("1\n2")
+	for i, line := range scan(input, bufio.ScanLines) {
+		fmt.Println(i, line)
+	}
+	// Output:
+	// 0 1
+	// 1 2
+}
